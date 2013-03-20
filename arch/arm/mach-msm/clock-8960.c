22637e43da8a7e4ad02468998028d666032b9129
thicklizard <thicklizard@gmail.com>
2013-03-20 09:39:30 -0500

    messing with some gpu tables

--- arch/arm/mach-msm/clock-8960.c
+++ arch/arm/mach-msm/clock-8960.c
@@ -3388,11 +3388,13 @@ static struct clk_freq_tbl clk_tbl_gfx3d_8960[] = {
 	F_GFX3D(160000000, pll2, 1,  5),
 	F_GFX3D(177778000, pll2, 2,  9),
 	F_GFX3D(200000000, pll2, 1,  4),
-	F_GFX3D(266667000, pll2, 2,  7),
-	F_GFX3D(320000000, pll2, 1,  3),
-	F_GFX3D(400000000, pll2, 3,  8),
-	F_GFX3D(450000000, pll2, 1,  2),
-	F_GFX3D(512000000, pll2, 2,  5),
+	F_GFX3D(228571000, pll2, 2,  7),
+	F_GFX3D(266667000, pll2, 1,  3),
+	F_GFX3D(300000000, pll3, 3,  8),
+	F_GFX3D(320000000, pll2, 2,  5),
+	F_GFX3D(400000000, pll2, 1,  4),
+	F_GFX3D(450000000, pll2, 3,  8),
+	F_GFX3D(512000000, pll2, 3,  8),
 	F_END
 };
 
