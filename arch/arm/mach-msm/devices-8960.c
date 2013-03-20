644cb14e380bb716ce083d96261162148674ae69
thicklizard <thicklizard@gmail.com>
2013-03-20 11:42:03 -0500

    messing with some gpu tables

--- arch/arm/mach-msm/devices-8960.c
+++ arch/arm/mach-msm/devices-8960.c
@@ -3078,17 +3078,17 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		{
 			.gpu_freq = 512000000,
 			.bus_freq = 4,
-			.io_fraction = 33,
+			.io_fraction = 0,
 		},
 		{
 			.gpu_freq = 400000000,
 			.bus_freq = 3,
-			.io_fraction = 50
+			.io_fraction = 0
 		},
 		{
 			.gpu_freq = 300000000,
 			.bus_freq = 2,
-			.io_fraction = 100,
+			.io_fraction = 33,
 		},
 		{
 			.gpu_freq = 266667000,
@@ -3100,8 +3100,8 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.bus_freq = 0,
 		},
 	},
-	.init_level = 2,
-	.max_level= 2,
+	.init_level = 1,
+	.max_level= 0,
 	.num_levels = 5,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/12,
@@ -3164,21 +3164,13 @@ static struct kgsl_device_platform_data kgsl_2d0_pdata = {
 			.bus_freq = 3,
 		},
 		{
-		 	.gpu_freq = 200000000,
- 			.bus_freq = 2,
- 		},
- 		{
- 			.gpu_freq = 96000000,
- 			.bus_freq = 1,
- 		},
- 		{
  			.gpu_freq = 27000000,
  			.bus_freq = 0,
 		},
 	},
-	.init_level = 1,
-	.max_level= 2,
-	.num_levels = 5,
+	.init_level = 0,
+	.max_level= 0,
+	.num_levels = 3,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/12,
 	.nap_allowed = true,
@@ -3240,21 +3232,13 @@ static struct kgsl_device_platform_data kgsl_2d1_pdata = {
 			.bus_freq = 3,
 		},
 		{
-		 	.gpu_freq = 200000000,
- 			.bus_freq = 2,
- 		},
- 		{
- 			.gpu_freq = 96000000,
- 			.bus_freq = 1,
- 		},
- 		{
  			.gpu_freq = 27000000,
  			.bus_freq = 0,
 		},
 	},
-	.init_level = 1,
-	.max_level= 2,
-	.num_levels = 5,
+	.init_level = 0,
+	.max_level= 0,
+	.num_levels = 3,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/12,
 	.nap_allowed = true,
